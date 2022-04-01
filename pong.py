db18920e372cc571851c04fefba788ce3b1a5cd8
Shubhanshu Shukla <sogoodshubh@gmail.com>
2022-04-01 19:25:07 +0530

    Rainbow🌈

--- pong.py
+++ pong.py
@@ -10,9 +10,10 @@ FPS = 60
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-BLUE = (0,0,128)
+BLUE = (0,0,255)
 RED = 	(255,0,0)
 GOLD = 	(255,215,0)
+LIME = 	(0,255,0)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_RADIUS = 7
@@ -21,8 +22,34 @@ SCORE_FONT = pygame.font.SysFont("comicsans", 50)
 WINNING_SCORE = 10
 
 
-class Paddle:
+class PaddleL:
     COLOR = RED
+    VEL = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+
+
+
+class PaddleR:
+    COLOR = BLUE
     VEL = 4
 
     def __init__(self, x, y, width, height):
@@ -74,8 +101,8 @@ class Ball:
 def draw(win, paddles, ball, left_score, right_score):
     win.fill(BLACK)
 
-    left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
-    right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    left_score_text = SCORE_FONT.render(f"{left_score}", 1, LIME)
+    right_score_text = SCORE_FONT.render(f"{right_score}", 1, LIME)
     win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
     win.blit(right_score_text, (WIDTH * (3/4) -
                                 right_score_text.get_width()//2, 20))
@@ -137,9 +164,9 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT //
+    left_paddle = PaddleL(10, HEIGHT//2 - PADDLE_HEIGHT //
                          2, PADDLE_WIDTH, PADDLE_HEIGHT)
-    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT //
+    right_paddle = PaddleR(WIDTH - 10 - PADDLE_WIDTH, HEIGHT //
                           2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
 
